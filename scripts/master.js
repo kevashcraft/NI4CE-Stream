f58665757deb0b83adbd067ef986a7d9fa418778
Kevin Ashcraft <kevin@kevashcraft.com>
2016-12-06 00:42:43 -0500

    styling

--- scripts/master.js
+++ scripts/master.js
@@ -1,23 +1,3 @@
-WebFontConfig = {
-  google: {
-    families: ['Hind']
-  }
-};
-
-(function(d) {
-  var wf = d.createElement('script'), s = d.scripts[0];
-  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-  s.parentNode.insertBefore(wf, s);
-})(document);
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-88266216-2', 'auto');
-ga('send', 'pageview');
-
 var NS = {
   live_label: document.getElementById('live_label'),
   play_label: document.getElementById('play_label'),
